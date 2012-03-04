4b5ea29667a4662f15cbf566b3b5090e1087fe9e
JungJoo Seo <moretajoo@gmail.com>
2012-03-05 08:47:11 +0900

    active record

---
+++ ruby-basic-study/beginning-rails/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c1c3d187ec00c5940d95b9a79d6be287c40c3ea667a3d565654059daea7e919325fa842e3ce4123ff24fba5c664416949ba8126fab9a18d87392d3d917b52acb'
---
+++ ruby-basic-study/beginning-rails/blog/db/migrate/20120303172742_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+
+      t.timestamps
+
+      t.string :title
+      t.text :body
+      t.datetime :published_at
+    end
+  end
+end
