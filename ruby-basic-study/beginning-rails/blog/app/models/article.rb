e3854136db13626a0ac92fad246b230ae16e246f
JungJoo Seo <moretajoo@gmail.com>
2012-03-09 02:51:17 +0900

    chapter4 end

--- ruby-basic-study/beginning-rails/blog/app/models/article.rb
+++ ruby-basic-study/beginning-rails/blog/app/models/article.rb
@@ -2,7 +2,20 @@ class Article < ActiveRecord::Base
   validates :title, :presence => true
   validates :body, :presence => true
 
+  belongs_to :user
+  has_and_belongs_to_many :categories
+  has_many :comments
+
+  scope :published, where("articles.publised_at IS NOT NULL")
+  scope :draft, where("articles.publised_at IS NULL")
+  scope :recent, lambda { published.where("articles.published_at > ?", 1.week.ago.to_date)}
+  scope :where_title, lambda { |term| where("articles.title LIKE ?", "%#{term}%")}
+
   def long_title
     "#{title} - #{published_at}"
+  end
+
+  def published?
+    published_at.present?
   end
 end
--- ruby-basic-study/beginning-rails/blog/db/schema.rb
+++ ruby-basic-study/beginning-rails/blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120304234311) do
+ActiveRecord::Schema.define(:version => 20120308164859) do
 
   create_table "articles", :force => true do |t|
     t.datetime "created_at",   :null => false
@@ -24,7 +24,27 @@ ActiveRecord::Schema.define(:version => 20120304234311) do
     t.integer  "user_id"
   end
 
+  create_table "articles_categories", :id => false, :force => true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "attachments", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.integer  "article_id"
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -47,9 +67,9 @@ ActiveRecord::Schema.define(:version => 20120304234311) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "hashed_password"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
