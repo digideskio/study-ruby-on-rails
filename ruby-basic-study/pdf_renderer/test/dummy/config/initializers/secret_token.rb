5f69c3d3e91dfbe7df11c2d7ef66a17037153282
JungJoo Seo <moretajoo@gmail.com>
2012-04-03 02:02:11 +0900

    study start metaprogramming ruby like pro

---
+++ ruby-basic-study/pdf_renderer/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '052cb0678f33c593690736eead94b6c725ba5907044e7b29f10ddc39920d4c9617d32b146097d43495205c3d0d5785c252999877111bb153decdf42ab022a983'
